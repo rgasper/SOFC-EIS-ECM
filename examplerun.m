4a0d7c617fbac7720ef622e47591df7875e05900
Raymond Gasper <raymond.j.gasper@gmail.com>
2018-02-28 17:23:32 -0500

    Update examplerun.m

--- examplerun.m
+++ examplerun.m
@@ -2,17 +2,17 @@
 %Ray Gasper, 2018, UMass Amherst
 clear;clc
 
-%% simplest possible run- no initial guess, no constraints
+%% simplest possible run- random initial guess, default constraints
 %If you run this multiple times you'll notice the ECM can change
 %significantly, this means there are many local minima in the
 %error:parameter space- only if you're lucky will this fit be realistic
 %sometimes it won't converge at all, often (not always) meaning the random 
-%initial guess is in a particularly bad region of parameter space
+%initial guess is in a particularly bad region of parameter space, and the fit is bad
 figure(1)
 [fit_1, err_1] = fit_eis_dat('exp_data_fine.csv',[],[],[]);
 title('Random Initial Guess & Default UB, LB')
 
-%% with initial guess
+%% with good initial guess, default UB and LB
 %this is using a pretty good initial guess.
 %Notice with re-running there's some, but little change- we're close to a good
 %minima, with these values listed corresponding to realistic ones
@@ -21,18 +21,19 @@ figure(2)
 [fit_2, err_2] = fit_eis_dat('exp_data_fine.csv',guess,[],[]);
 title('Good Initial Guess & Default UB, LB')
 
-%% with bad UB and LB, no initial guess
-% you need very good upper bounds and lower bounds, see the result of a poor set here
-%just arbitrarily setting upper bounds on all the elements almost always
+%% with bad UB and LB, random initial guess
+% you need good upper bounds and lower bounds, see the result of a poor set here
+%just arbitrarily setting bounds on all the parameters almost always
 %causes failure to produce a good set
 ub=[0.1,0.1,0.99,0.5,0.5,0.99,0.5,0.5,0.5,0.5,0.99,0.99];
-lb=[0,0,0,0,0,0,0,0,0,0,0,0];
+lb=[0,0,0,0,0,0,0.1,0.1,0,0,0,0];
 figure(3)
 [fit_3, err_3] = fit_eis_dat('exp_data_fine.csv',[],ub,lb);
 title('Random Initial Guess & BAD Custom UB, LB')
 
-%% with good UB, no initial guess
-ub = [1 1 1 1 1 1 1 1 0.2 1 1 1]; %We're limiting the resistance of the GE element
+%% with good UB, random initial guess
+%We're setting UB for the resistance of the GE element, and leaving everything else default
+ub = [1 1 1 1 1 1 1 1 0.2 1 1 1]; 
 figure(4)
-[fit_4, err_4] = fit_eis_dat('exp_data_fine.csv',[],ub,lb);
+[fit_4, err_4] = fit_eis_dat('exp_data_fine.csv',[],ub,[]);
 title('Random Initial Guess & Good Custom UB, Default LB')
